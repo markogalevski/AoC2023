e2d4e2eca614bb5d40c8507a89eb12b0b355054e
Marko Galevski <galevski.marko+github@gmail.com>
2024-01-17 23:21:31 +0200

    Solved part 1 for day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    println!("{}", run("input.txt"));
+}
+
+fn run(filename: &str) -> i64 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut sums: i64 = 0;
+    for line in reader.lines() {
+        let mut histories: Vec<Vec<i64>> = vec![];
+        let line = line.unwrap();
+        let mut history: Vec<i64> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        histories.push(history.clone());
+        while !history.iter().all(|x| *x == 0) {
+            history = history.clone().windows(2).map(|sl| sl[1] - sl[0]).collect();
+            histories.push(history.clone());
+        }
+        sums += histories
+            .iter()
+            .map(|history| history.iter().last().unwrap())
+            .sum::<i64>();
+    }
+    sums
+}
+
+#[test]
+fn sample_test() {
+    assert_eq!(run("sample_input.txt"), 114);
+}
