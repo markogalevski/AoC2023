34fe64b1ac141b88ea374d9f6ad7560286c62631
Marko Galevski <galevski.marko+github@gmail.com>
2024-01-17 09:09:39 +0100

    BLAZING 🚀🚀🚀 fast solution to day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,14 +1,71 @@
-use std::{cmp::Ordering, fs::File, io::BufRead, io::BufReader};
+use std::{fs::File, io::BufRead, io::BufReader};
 
 fn main() {
     let result = run("input.txt");
     println!("result = {result}");
 }
 
+type SeedRange = std::ops::Range<usize>;
+
+fn add_offset(seed_range: SeedRange, offset: i64) -> SeedRange {
+    usize::try_from(seed_range.start as i64 + offset).unwrap()
+        ..usize::try_from(seed_range.end as i64 + offset).unwrap()
+}
+
+fn convert_seed_range(seed_range: SeedRange, map: &FarmingMap, seed_ranges: &mut Vec<SeedRange>) {
+    for range in &map.ranges {
+        //case 1 - full fit
+        if seed_range.start >= range.start && seed_range.end <= range.end {
+            seed_ranges.push(add_offset(seed_range, range.conversion_offset));
+            return;
+        }
+        //case 2.a - partial fit left
+        else if seed_range.start < range.start
+            && seed_range.end > range.start
+            && seed_range.end < range.end
+        {
+            let external_range = seed_range.start..range.start;
+            convert_seed_range(external_range, &map, seed_ranges);
+            let internal_range = range.start..seed_range.end;
+            seed_ranges.push(add_offset(internal_range, range.conversion_offset));
+            return;
+        }
+        //case 2.b - partial fit right
+        else if seed_range.start > range.start
+            && seed_range.start < range.end
+            && seed_range.end > range.end
+        {
+            let external_range = range.end..seed_range.end;
+            convert_seed_range(external_range, &map, seed_ranges);
+            let internal_range = seed_range.start..range.end;
+            seed_ranges.push(add_offset(internal_range, range.conversion_offset));
+            return;
+        }
+        //case 2.c - double-ended partial fit
+        else if seed_range.start < range.start && seed_range.end > (range.end) {
+            let left_external_range = seed_range.start..range.start;
+            convert_seed_range(left_external_range, &map, seed_ranges);
+            let right_external_range = range.end..seed_range.end;
+            convert_seed_range(right_external_range, &map, seed_ranges);
+            let internal_range = range.start..range.end;
+            seed_ranges.push(add_offset(internal_range, range.conversion_offset));
+            return;
+        }
+        //case 3 - no fit at all
+        else if (seed_range.start < range.start && seed_range.end < range.end)
+            || (seed_range.start > range.start && seed_range.end > range.end)
+        {
+            continue;
+        }
+    }
+    seed_ranges.push(seed_range);
+    return;
+}
+
 #[derive(Debug)]
 struct FarmingRange {
-    source: usize,
-    length: usize,
+    start: usize,
+    end: usize,
     conversion_offset: i64,
 }
 
@@ -18,21 +75,18 @@ impl FarmingRange {
         let destination: i64 = split[0]
             .parse()
             .expect(&format!("Unable to unwrap destination {}", split[0]));
-        let source = split[1]
+        let start = split[1]
             .parse()
-            .expect(&format!("Unable to unwrap source {}", split[1]));
-        let length = split[2]
+            .expect(&format!("Unable to unwrap start {}", split[1]));
+        let length: usize = split[2]
             .parse()
             .expect(&format!("Unable to unwrap length {}", split[2]));
+        let end = start + length;
         Self {
-            source,
-            length,
-            conversion_offset: (destination - source as i64),
+            start,
+            end,
+            conversion_offset: (destination - start as i64),
         }
-    }
-
-    fn process_source(&self, value: usize) -> usize {
-        (value as i64 + self.conversion_offset) as usize
     }
 }
 
@@ -64,7 +118,7 @@ fn run(filename: &str) -> usize {
     let lines = BufReader::new(file).lines();
 
     let mut collection_state = CollectingData::Seeds;
-    let mut seed_vec: Vec<usize> = vec![];
+    let mut seed_ranges: Vec<SeedRange> = vec![];
     let mut seed_to_soil = FarmingMap::new();
     let mut soil_to_fertilizer = FarmingMap::new();
     let mut fertilizer_to_water = FarmingMap::new();
@@ -103,9 +157,7 @@ fn run(filename: &str) -> usize {
                         .map(|s| s.parse().expect(&format!("Unable to unwrap {s}")))
                         .collect();
                     for pair in pairs_list.chunks(2) {
-                        for num in 0..pair[1] {
-                            seed_vec.push(pair[0] + num);
-                        }
+                        seed_ranges.push(pair[0]..(pair[0] + pair[1]));
                     }
                 }
             }
@@ -139,32 +191,39 @@ fn run(filename: &str) -> usize {
     ];
     for map in maps.iter_mut() {
         map.ranges
-            .sort_by(|a, b| a.source.partial_cmp(&b.source).unwrap());
+            .sort_by(|a, b| a.start.partial_cmp(&b.start).unwrap());
     }
-    let mut seed_values: Vec<usize> = vec![];
-    for seed in seed_vec {
-        let mut seed_value = seed;
-        for map in &maps {
-            if let Ok(idx) = map.ranges.binary_search_by(|range| {
-                if range.source > seed_value {
-                    Ordering::Greater
-                } else if (range.source + range.length) <= seed_value {
-                    Ordering::Less
-                } else {
-                    Ordering::Equal
-                }
-            }) {
-                seed_value = map.ranges[idx].process_source(seed_value);
-            }
+    let mut input_seed_ranges = seed_ranges.clone();
+    let mut output_seed_ranges: Vec<SeedRange> = vec![];
+    for map in maps.iter() {
+        for seed_range in input_seed_ranges {
+            convert_seed_range(seed_range, map, &mut output_seed_ranges);
         }
-        seed_values.push(seed_value);
+        input_seed_ranges = output_seed_ranges.clone();
+        sort_seed_ranges(&mut input_seed_ranges);
+        output_seed_ranges.clear();
     }
-    seed_values.sort();
-    seed_values[0]
+    sort_seed_ranges(&mut input_seed_ranges);
+
+    input_seed_ranges[0].start
+}
+
+fn sort_seed_ranges(seed_ranges: &mut Vec<SeedRange>) {
+    seed_ranges.sort_by(|a, b| {
+        let result = a.start.partial_cmp(&b.start).unwrap();
+        match result {
+            std::cmp::Ordering::Equal => a.end.partial_cmp(&b.end).unwrap(),
+            _ => result,
+        }
+    });
 }
 
 #[test]
 fn sample_test() {
-    // assert_eq!(run("sample_input.txt"), 35);
     assert_eq!(run("sample_input.txt"), 46);
+}
+
+#[test]
+fn main_test() {
+    assert_eq!(run("input.txt"), 15290096);
 }
