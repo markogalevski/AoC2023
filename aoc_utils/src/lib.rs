5340bfdb14aecfb05babe06b529a76b51348167e
Marko Galevski <galevski.marko+github@gmail.com>
2024-02-12 01:03:34 +0200

    working solution for part1 day18, reusing day10 shoelace code

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -1,39 +1,97 @@
-pub type Matrix = Vec<Vec<char>>;
+pub mod matrix {
+    pub type Matrix = Vec<Vec<char>>;
 
-pub fn matrix_transpose(matrix: Matrix) -> Matrix {
-    let new_row: Vec<char> = vec!['x'; matrix.len()];
-    let mut new_matrix: Matrix = vec![new_row; matrix[0].len()];
-    matrix.iter().enumerate().for_each(|(i, row)| {
-        row.iter().enumerate().for_each(|(j, element)| {
-            new_matrix[j][i] = *element;
-        })
-    });
-    new_matrix
+    pub fn matrix_transpose(matrix: Matrix) -> Matrix {
+        let new_row: Vec<char> = vec!['x'; matrix.len()];
+        let mut new_matrix: Matrix = vec![new_row; matrix[0].len()];
+        matrix.iter().enumerate().for_each(|(i, row)| {
+            row.iter().enumerate().for_each(|(j, element)| {
+                new_matrix[j][i] = *element;
+            })
+        });
+        new_matrix
+    }
+
+    pub fn matrix_invert_rows(matrix: Matrix) -> Matrix {
+        let mut new_matrix = matrix.clone();
+        new_matrix.reverse();
+        new_matrix
+    }
+
+    pub fn matrix_invert_cols(matrix: Matrix) -> Matrix {
+        let mut new_matrix = matrix.clone();
+        new_matrix.iter_mut().for_each(|row| row.reverse());
+        new_matrix
+    }
+
+    pub fn matrix_rotate_ccw(matrix: Matrix) -> Matrix {
+        matrix_invert_rows(matrix_transpose(matrix))
+    }
+
+    pub fn matrix_rotate_cw(matrix: Matrix) -> Matrix {
+        matrix_invert_cols(matrix_transpose(matrix))
+    }
 }
 
-pub fn matrix_invert_rows(matrix: Matrix) -> Matrix {
-    let mut new_matrix = matrix.clone();
-    new_matrix.reverse();
-    new_matrix
-}
+pub mod polygon {
+    pub type Polygon = Vec<Point>;
 
-pub fn matrix_invert_cols(matrix: Matrix) -> Matrix {
-    let mut new_matrix = matrix.clone();
-    new_matrix.iter_mut().for_each(|row| row.reverse());
-    new_matrix
-}
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Point {
+        pub row: i64,
+        pub col: i64,
+    }
 
-pub fn matrix_rotate_ccw(matrix: Matrix) -> Matrix {
-    matrix_invert_rows(matrix_transpose(matrix))
-}
+    impl Point {
+        pub fn new(row: i64, col: i64) -> Self {
+            Self { row, col }
+        }
+        pub fn add(&self, other: &Self) -> Self {
+            Self {
+                row: self.row + other.row,
+                col: self.col + other.col,
+            }
+        }
+    }
 
-pub fn matrix_rotate_cw(matrix: Matrix) -> Matrix {
-    matrix_invert_cols(matrix_transpose(matrix))
+    pub fn circumference(polygon: &Polygon) -> f64 {
+        let mut circ = polygon
+            .windows(2)
+            .map(|coords| (coords[0].row, coords[1].row, coords[0].col, coords[1].col))
+            .map(|(x1, x2, y1, y2)| (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) as f64).sqrt())
+            .sum();
+        let last = polygon.last().unwrap();
+        let first = polygon.first().unwrap();
+        circ += (((first.row - last.row) * (first.row - last.row)
+            + (first.col - last.col) * (first.col - last.col)) as f64)
+            .sqrt();
+        circ
+    }
+
+    pub fn shoelace(polygon: &Polygon) -> f64 {
+        let mut polygon_copy = polygon.clone();
+        let start_coords = polygon[0];
+        polygon_copy.push(start_coords);
+        (polygon_copy
+            .windows(2)
+            .map(|coords| (coords[0].row, coords[0].col, coords[1].row, coords[1].col))
+            .map(|(y1, x1, y2, x2)| x1 * y2 - y1 * x2)
+            .sum::<i64>() as f64
+            * 0.5)
+            .abs()
+            .ceil()
+    }
+
+    pub fn picks_theorem_num_internal_points(area: f64, num_boundary_points: i64) -> i64 {
+        println!("A = {area}, b = {num_boundary_points}");
+        (area + 1. - num_boundary_points as f64 * 0.5) as i64
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::matrix::*;
+    use super::polygon::*;
 
     #[test]
     fn test_matrix_transpose() {
@@ -101,5 +159,21 @@ mod tests {
         ];
 
         assert_eq!(matrix_t, reference);
+    }
+    #[test]
+    fn test_shoelace() {
+        let polygon: Vec<Point> = vec![
+            Point { row: 1, col: 2 },
+            Point { row: 3, col: 1 },
+            Point { row: 2, col: 4 },
+            Point { row: 4, col: 6 },
+            Point { row: 0, col: 5 },
+        ];
+        assert_eq!(shoelace(&polygon), 8.);
+    }
+
+    #[test]
+    fn test_picks() {
+        assert_eq!(picks_theorem_num_internal_points(10., 8), 7)
     }
 }
---
+++ day18/src/main.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use aoc_utils::polygon::{
+    circumference, picks_theorem_num_internal_points, shoelace, Point, Polygon,
+};
+
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl std::convert::From<&str> for Direction {
+    fn from(s: &str) -> Self {
+        match s {
+            "R" => Self::Right,
+            "U" => Self::Up,
+            "D" => Self::Down,
+            "L" => Self::Left,
+            &_ => panic!("unexpected direction!"),
+        }
+    }
+}
+
+fn main() {
+    println!("{}", run("input.txt"));
+}
+
+fn run(filename: &str) -> i64 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let start_point = Point::new(0, 0);
+    let mut polygon: Polygon = vec![start_point];
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let line: Vec<&str> = line.split_whitespace().collect();
+        let direction = Direction::from(line[0]);
+        let steps: i64 = line[1].parse().unwrap();
+        let latest_point = polygon.iter().last().unwrap();
+        let offset = compute_offset(direction, steps);
+        polygon.push(latest_point.add(&offset));
+    }
+    let area = shoelace(&polygon);
+    let circumference = circumference(&polygon) as i64;
+    let internal_points = picks_theorem_num_internal_points(area, circumference);
+    circumference + internal_points
+}
+
+fn compute_offset(dir: Direction, steps: i64) -> Point {
+    let mut offset = Point::new(0, 0);
+    match dir {
+        Direction::Right => offset.col += steps,
+        Direction::Left => offset.col -= steps,
+        Direction::Up => offset.row -= steps,
+        Direction::Down => offset.row += steps,
+    };
+    offset
+}
+
+#[test]
+fn test_sample() {
+    assert_eq!(run("sample_input.txt"), 62);
+}
